35b25f9df8cd4a3d323b94d0ac788d52bb901a96
Andrei Solodov <andrei.solodov@applovin.com>
2025-07-14 13:01:27 +0300

    log directive before

--- language/internal/cc/parser/parser.go
+++ language/internal/cc/parser/parser.go
@@ -445,8 +445,8 @@ func (p *parser) parseDirectivesUntil(shouldStop func(token string) bool) ([]Dir
 
 		switch {
 		case strings.HasPrefix(token, "#"):
+			fmt.Printf("+++%s+++\n", token)
 			directive, err := p.parseDirective(token)
-			fmt.Printf("+++%v+++\n", directive)
 			if err != nil {
 				p.skipLine()
 				// log.Printf("Failed to parse %v directive: %v, skipping tokens until end of line: %v", token, err, skipped)
